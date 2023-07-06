5e6a09f63ff8d2063412e52212352f2f36f48db1
Claire Lee <lee.claire156@gmail.com>
2023-07-05 21:41:09 -0400

    Reinitializes with Expo

---
+++ app/index.js
@@ -0,0 +1,12 @@
+// Think of View as a div and Text as a p or heading element
+import {View, Text} from 'react-native';
+
+const Home = () => {
+    return(
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;

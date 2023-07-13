35f7bbc26749aa14a0fc17fcd215a0841e694a03
Claire Lee <lee.claire156@gmail.com>
2023-07-12 23:54:00 -0400

    Separates login to its own file

--- app/index.js
+++ app/index.js
@@ -1,15 +1,23 @@
 // Think of View as a div and Text as a p or heading element
-import {SafeAreaView, Text} from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
-import React, {useState} from 'react';
+import { View, SafeAreaView, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './login';
 
 const Home = () => {
-    return(
-        <SafeAreaView>
-            <Text>Home</Text>
-            <TextInput defaultValue='Username'></TextInput>
-            <TextInput defaultValue='Password'></TextInput>
-        </SafeAreaView>
+    const Stack = createNativeStackNavigator();
+
+    return (
+
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name='login'
+                    component={Login}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+
     )
 }
 
